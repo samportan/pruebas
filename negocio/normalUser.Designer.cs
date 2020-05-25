0b9dfed11e472802fdc94353346b78e2bfc2673c
DanielQuesada97 <00147619@uca.edu.sv>
2020-05-24 20:34:41 -0600

    fix bin files

--- negocio/normalUser.Designer.cs
+++ negocio/normalUser.Designer.cs
@@ -33,25 +33,25 @@ namespace negocio
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.orderTab = new System.Windows.Forms.TabPage();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblQuantity = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.lblProductName = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonBuy = new System.Windows.Forms.Button();
+            this.numericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.ProductComboBox = new System.Windows.Forms.ComboBox();
+            this.labelTotal = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.historyTab = new System.Windows.Forms.TabPage();
-            this.label2 = new System.Windows.Forms.Label();
-            this.ProductComboBox = new System.Windows.Forms.ComboBox();
-            this.numericUpDown = new System.Windows.Forms.NumericUpDown();
-            this.buttonBuy = new System.Windows.Forms.Button();
-            this.label3 = new System.Windows.Forms.Label();
-            this.labelTotal = new System.Windows.Forms.Label();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.lblProductName = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.lblQuantity = new System.Windows.Forms.Label();
-            this.lblDescription = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
             this.orderTab.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize) (this.numericUpDown)).BeginInit();
-            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -80,6 +80,55 @@ namespace negocio
             this.orderTab.TabIndex = 0;
             this.orderTab.Text = "Pedidos";
             // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.SlateGray;
+            this.groupBox2.Controls.Add(this.lblQuantity);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.lblDescription);
+            this.groupBox2.Controls.Add(this.lblProductName);
+            this.groupBox2.Location = new System.Drawing.Point(32, 218);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(546, 154);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Datos del Producto";
+            // 
+            // lblQuantity
+            // 
+            this.lblQuantity.BackColor = System.Drawing.Color.Transparent;
+            this.lblQuantity.Location = new System.Drawing.Point(78, 56);
+            this.lblQuantity.Name = "lblQuantity";
+            this.lblQuantity.Size = new System.Drawing.Size(56, 18);
+            this.lblQuantity.TabIndex = 1;
+            this.lblQuantity.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblQuantity.Click += new System.EventHandler(this.lblQuantity_Click);
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(16, 56);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 18);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Cantidad:";
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.BackColor = System.Drawing.Color.Transparent;
+            this.lblDescription.Location = new System.Drawing.Point(16, 74);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(509, 66);
+            this.lblDescription.TabIndex = 0;
+            this.lblDescription.Click += new System.EventHandler(this.lblDescription_Click);
+            // 
+            // lblProductName
+            // 
+            this.lblProductName.Location = new System.Drawing.Point(16, 27);
+            this.lblProductName.Name = "lblProductName";
+            this.lblProductName.Size = new System.Drawing.Size(144, 20);
+            this.lblProductName.TabIndex = 0;
+            this.lblProductName.Click += new System.EventHandler(this.lblProductName_Click);
+            // 
             // groupBox1
             // 
             this.groupBox1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
@@ -97,6 +146,63 @@ namespace negocio
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Datos de Compra";
+            // 
+            // buttonBuy
+            // 
+            this.buttonBuy.BackColor = System.Drawing.Color.Gold;
+            this.buttonBuy.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.buttonBuy.Location = new System.Drawing.Point(439, 33);
+            this.buttonBuy.Name = "buttonBuy";
+            this.buttonBuy.Size = new System.Drawing.Size(82, 93);
+            this.buttonBuy.TabIndex = 3;
+            this.buttonBuy.Text = "Comprar";
+            this.buttonBuy.UseVisualStyleBackColor = false;
+            // 
+            // numericUpDown
+            // 
+            this.numericUpDown.Location = new System.Drawing.Point(232, 67);
+            this.numericUpDown.Name = "numericUpDown";
+            this.numericUpDown.Size = new System.Drawing.Size(175, 20);
+            this.numericUpDown.TabIndex = 2;
+            this.numericUpDown.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // ProductComboBox
+            // 
+            this.ProductComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ProductComboBox.FormattingEnabled = true;
+            this.ProductComboBox.Location = new System.Drawing.Point(232, 40);
+            this.ProductComboBox.Name = "ProductComboBox";
+            this.ProductComboBox.Size = new System.Drawing.Size(175, 21);
+            this.ProductComboBox.TabIndex = 1;
+            // 
+            // labelTotal
+            // 
+            this.labelTotal.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.labelTotal.Location = new System.Drawing.Point(232, 95);
+            this.labelTotal.Name = "labelTotal";
+            this.labelTotal.Size = new System.Drawing.Size(175, 26);
+            this.labelTotal.TabIndex = 0;
+            this.labelTotal.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label3.Location = new System.Drawing.Point(33, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(166, 31);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Total:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label2.Location = new System.Drawing.Point(33, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(166, 31);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Cantidad a comprar:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // label1
             // 
@@ -119,112 +225,6 @@ namespace negocio
             this.historyTab.Text = "Historial de Compras";
             this.historyTab.UseVisualStyleBackColor = true;
             // 
-            // label2
-            // 
-            this.label2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
-            this.label2.Location = new System.Drawing.Point(33, 64);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(166, 31);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "Cantidad a comprar:";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            // 
-            // ProductComboBox
-            // 
-            this.ProductComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.ProductComboBox.FormattingEnabled = true;
-            this.ProductComboBox.Location = new System.Drawing.Point(232, 40);
-            this.ProductComboBox.Name = "ProductComboBox";
-            this.ProductComboBox.Size = new System.Drawing.Size(175, 21);
-            this.ProductComboBox.TabIndex = 1;
-            // 
-            // numericUpDown
-            // 
-            this.numericUpDown.Location = new System.Drawing.Point(232, 67);
-            this.numericUpDown.Name = "numericUpDown";
-            this.numericUpDown.Size = new System.Drawing.Size(175, 20);
-            this.numericUpDown.TabIndex = 2;
-            this.numericUpDown.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
-            // 
-            // buttonBuy
-            // 
-            this.buttonBuy.BackColor = System.Drawing.Color.Gold;
-            this.buttonBuy.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
-            this.buttonBuy.Location = new System.Drawing.Point(439, 33);
-            this.buttonBuy.Name = "buttonBuy";
-            this.buttonBuy.Size = new System.Drawing.Size(82, 93);
-            this.buttonBuy.TabIndex = 3;
-            this.buttonBuy.Text = "Comprar";
-            this.buttonBuy.UseVisualStyleBackColor = false;
-            // 
-            // label3
-            // 
-            this.label3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
-            this.label3.Location = new System.Drawing.Point(33, 95);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(166, 31);
-            this.label3.TabIndex = 0;
-            this.label3.Text = "Total:";
-            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            // 
-            // labelTotal
-            // 
-            this.labelTotal.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
-            this.labelTotal.Location = new System.Drawing.Point(232, 95);
-            this.labelTotal.Name = "labelTotal";
-            this.labelTotal.Size = new System.Drawing.Size(175, 26);
-            this.labelTotal.TabIndex = 0;
-            this.labelTotal.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            // 
-            // groupBox2
-            // 
-            this.groupBox2.BackColor = System.Drawing.Color.SlateGray;
-            this.groupBox2.Controls.Add(this.lblQuantity);
-            this.groupBox2.Controls.Add(this.label5);
-            this.groupBox2.Controls.Add(this.lblDescription);
-            this.groupBox2.Controls.Add(this.lblProductName);
-            this.groupBox2.Location = new System.Drawing.Point(32, 218);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(546, 154);
-            this.groupBox2.TabIndex = 1;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Datos del Producto";
-            // 
-            // lblProductName
-            // 
-            this.lblProductName.Location = new System.Drawing.Point(16, 27);
-            this.lblProductName.Name = "lblProductName";
-            this.lblProductName.Size = new System.Drawing.Size(144, 20);
-            this.lblProductName.TabIndex = 0;
-            this.lblProductName.Click += new System.EventHandler(this.lblProductName_Click);
-            // 
-            // label5
-            // 
-            this.label5.Location = new System.Drawing.Point(16, 56);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(65, 18);
-            this.label5.TabIndex = 1;
-            this.label5.Text = "Cantidad:";
-            // 
-            // lblQuantity
-            // 
-            this.lblQuantity.BackColor = System.Drawing.Color.Transparent;
-            this.lblQuantity.Location = new System.Drawing.Point(78, 56);
-            this.lblQuantity.Name = "lblQuantity";
-            this.lblQuantity.Size = new System.Drawing.Size(56, 18);
-            this.lblQuantity.TabIndex = 1;
-            this.lblQuantity.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.lblQuantity.Click += new System.EventHandler(this.lblQuantity_Click);
-            // 
-            // lblDescription
-            // 
-            this.lblDescription.BackColor = System.Drawing.Color.Transparent;
-            this.lblDescription.Location = new System.Drawing.Point(16, 74);
-            this.lblDescription.Name = "lblDescription";
-            this.lblDescription.Size = new System.Drawing.Size(509, 66);
-            this.lblDescription.TabIndex = 0;
-            this.lblDescription.Click += new System.EventHandler(this.lblDescription_Click);
-            // 
             // normalUser
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -238,9 +238,9 @@ namespace negocio
             this.Text = "normalUser";
             this.tabControl1.ResumeLayout(false);
             this.orderTab.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
             this.groupBox1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize) (this.numericUpDown)).EndInit();
-            this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
         }
 
